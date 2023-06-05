2d98cf62470cc403a83513bdec72ee366bb36459
scriabinEtude <scriabinetude87@gmail.com>
2023-06-05 21:13:02 +0900

    init

---
+++ wifitalk/wifitalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  wifitalkApp.swift
+//  wifitalk
+//
+//  Created by escher on 2023/06/05.
+//
+
+import SwiftUI
+
+@main
+struct wifitalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
